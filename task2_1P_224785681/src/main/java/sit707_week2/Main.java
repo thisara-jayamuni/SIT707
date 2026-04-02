47d65501a629b794f02e064e1f034a839a767641
Thisara Jayamuni <thisara.jayamuni95@gmail.com>
2026-04-02 19:17:48 +1100

    task 2.1P - test a registration pages using Selenium

---
+++ task2_1P_224785681/src/main/java/sit707_week2/Main.java
@@ -0,0 +1,15 @@
+package sit707_week2;
+
+/**
+ * Hello world!
+ * @author Ahsan Habib
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
+        
+        SeleniumOperations.github_registration_page("https://github.com/signup");
+    }
+}
---
+++ task2_1P_224785681/src/main/java/sit707_week2/SeleniumOperations.java
@@ -0,0 +1,162 @@
+package sit707_week2;
+
+import java.io.File;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import com.google.common.io.Files;
+
+
+/**
+ * This class demonstrates Selenium locator APIs to identify HTML elements.
+ * 
+ * Details in Selenium documentation https://www.selenium.dev/documentation/webdriver/elements/locators/
+ * 
+ * @author Ahsan Habib
+ */
+public class SeleniumOperations {
+
+	public static void sleep(int sec) {
+		try {
+			Thread.sleep(sec*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void officeworks_registration_page(String url) {
+		// Step 1: Locate chrome driver folder in the local drive.
+		System.setProperty("webdriver.chrome.driver", "C:/Users/thisa/Downloads/Compressed/chromedriver-win64/chromedriver-win64/chromedriver.exe");
+		
+		// Step 2: Use above chrome driver to open up a chromium browser.
+		System.out.println("Fire up chrome browser.");
+		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("Driver info: " + driver);
+		
+		sleep(2);
+	
+		// Load a webpage in chromium browser.
+		driver.get(url);
+		
+		/*
+		 * How to identify a HTML input field -
+		 * Step 1: Inspect the webpage, 
+		 * Step 2: locate the input field, 
+		 * Step 3: Find out how to identify it, by id/name/...
+		 */
+		
+		// Find first input field which is firstname
+		WebElement element = driver.findElement(By.id("firstname"));
+		System.out.println("Found element: " + element);
+		// Send first name
+		element.sendKeys("Thisara");
+		
+		/*
+		 * Find following input fields and populate with values
+		 */
+		WebElement lastName = driver.findElement(By.id("lastname"));
+		lastName.sendKeys("Jayamuni");
+		
+		WebElement phoneNumber = driver.findElement(By.id("phoneNumber"));
+		phoneNumber.sendKeys("0425123456");
+		
+		WebElement email = driver.findElement(By.id("email"));
+		email.sendKeys("thisara@gmail.com");
+		
+		WebElement password = driver.findElement(By.id("password"));
+		password.sendKeys("123456");
+		
+		WebElement confirmPassword = driver.findElement(By.id("confirmPassword"));
+		confirmPassword.sendKeys("123456");
+		
+		/*
+		 * Identify button 'Create account' and click to submit using Selenium API.
+		 */
+		 WebElement createAccountBtn = driver.findElement(
+	                By.xpath("//button[@type='submit']"));
+	        System.out.println("Found 'Create account' button: " + createAccountBtn);
+	        createAccountBtn.click();
+		
+		/*
+		 * Take screenshot using selenium API.
+		 */
+	        try {
+	            File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+	            File destFile = new File("D:/Acedemic/MSC - Deakin/2026 - T1/SIT707 - Software Quality and Testing/SIT707-2.1P/Screenshots/officeworks_registration.png");
+	            destFile.getParentFile().mkdirs();
+	            Files.copy(srcFile, destFile);
+	            System.out.println("Screenshot saved: " + destFile.getAbsolutePath());
+	        } catch (Exception e) {
+	            System.err.println("Failed to take screenshot: " + e.getMessage());
+	        }
+		
+		
+		// Sleep a while
+		sleep(2);
+		
+		// close chrome driver
+		driver.close();	
+	}
+	
+	
+	public static void github_registration_page(String url) {
+		// Step 1: Locate chrome driver folder in the local drive.
+		System.setProperty("webdriver.chrome.driver", "C:/Users/thisa/Downloads/Compressed/chromedriver-win64/chromedriver-win64/chromedriver.exe");
+		
+		// Step 2: Use above chrome driver to open up a chromium browser.
+		System.out.println("Fire up chrome browser.");
+		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("Driver info: " + driver);
+		
+		sleep(2);
+	
+		// Load a webpage in chromium browser.
+		driver.get(url);
+		
+		WebElement email = driver.findElement(By.id("email"));
+		email.sendKeys("thisara@gmail.com");
+		
+		WebElement password = driver.findElement(By.id("password"));
+		password.sendKeys("123456");
+		
+		WebElement username = driver.findElement(By.id("login"));
+		username.sendKeys("thisara-test");
+		
+        WebElement emailPrefCheckbox = driver.findElement(
+                By.id("user_signup[marketing_consent]"));
+        System.out.println("Found email preference checkbox: " + emailPrefCheckbox);
+        if (!emailPrefCheckbox.isSelected()) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", emailPrefCheckbox);
+        }
+        
+        WebElement createAccountBtn = driver.findElement(
+                By.xpath("//button[@data-target='signup-form.SignupButton']"));
+        System.out.println("Found 'Create account' button: " + createAccountBtn);
+        createAccountBtn.click();
+	
+        try {
+            File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            File destFile = new File("D:/Acedemic/MSC - Deakin/2026 - T1/SIT707 - Software Quality and Testing/SIT707-2.1P/Screenshots/github_registration.png");
+            destFile.getParentFile().mkdirs();
+            Files.copy(srcFile, destFile);
+            System.out.println("Screenshot saved: " + destFile.getAbsolutePath());
+        } catch (Exception e) {
+            System.err.println("Failed to take screenshot: " + e.getMessage());
+        }
+		
+		// Sleep a while
+		sleep(2);
+		
+		// close chrome driver
+		driver.close();	
+	}
+	
+}
