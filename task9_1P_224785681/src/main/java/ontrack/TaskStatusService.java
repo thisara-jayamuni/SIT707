eafdf3d90ca85b8ae5d5e61cc0010e65f0e7b803
Thisara Jayamuni <thisara.jayamuni95@gmail.com>
2026-05-20 12:59:19 +1000

    task 9.1P - test comment

--- task9_1P_224785681/src/main/java/ontrack/TaskStatusService.java
+++ task9_1P_224785681/src/main/java/ontrack/TaskStatusService.java
@@ -3,7 +3,7 @@ package ontrack;
 public class TaskStatusService {
 
 	public String getTaskStatus(String status) {
-    //	Test
+    //	Test  2
         if (status == null || status.trim().isEmpty()) {
             return "Invalid task status.";
         }
